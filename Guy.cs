d82e9bcd3a925ef538310fd85c5d23dee2616436
Administrateur <Administrateur@HP>
2024-03-27 11:36:43 +0000

    Ajoutez des fichiers projet.

---
+++ Guy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_guy
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+       
+        public void WriteMyInfo ()
+        {
+            Console.WriteLine (Name + "has " + Cash + "bucks." );
+        }
+        public int GiveCash( int amount )
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + "says:" + amount + "isn't a valid amount");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + "says:" + "I dont have enough cash to give you " + amount);
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+
+        }
+        public void ReceiveCash (int amount) {
+
+
+    }
+}
